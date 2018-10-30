c2ab4b56ce2bd54629425c126c0a2388520ab39f
yinwanng <xxjohnnyng@gmail.com>
2018-10-30 01:43:47 -0700

    added hangman code

---
+++ models/model.js
@@ -0,0 +1,48 @@
+var welcomeTitle = "Hangman Game";               // welcome message
+var winMessage = "You got it right!";            // display win message
+var loseMessage = "Game Over!";                  // display lose message
+var lifeCount = 7;                               // number of guesses limit
+var scoreCount = 0;                              // score of the game
+var guessWord;                                   // word to be guessed
+var lettersGuessedCorrect = [];                  // letters of the correct guesses
+var currentWord;                                 // current word displayed
+
+// the alphabet in an array
+const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 
+                'K', 'L', 'M', 'O', 'P', 'Q', 'R', 'S', 
+                'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+// dictionary of words with definition                
+var dictionary = [
+    {
+        word: "Tattoo",
+        definition: "A form of body modification where a design is made by inserting ink",
+    }, {
+        word: "Electricity",
+        definition: "Is the set of physical phenomena associated with the presence and motion of electric charge",
+    }, {
+        word: "Computer",
+        definition: "A programmable electronic device designed to accept data, perform prescribed mathematical and logical operations at high speed, and display the results of these operations.",
+    }, {
+        word: "Wallet",
+        definition: "A flat, folding pocketbook, especially one large enough to hold paper money, credit cards, driver's license, etc., and sometimes having a compartment for coins.",
+    }, {
+        word: "Car",
+        definition: "An automobile.",
+    }, {
+        word: "Cup",
+        definition: "A small, open container made of china, glass, metal, etc., usually having a handle and used chiefly as a receptable from which to drink tea, soup, etc.",
+    }, {
+        word: "Food",
+        definition: "Any nourishing substance that is eaten, drunk, or otherwise taken into the body to sustain life, provide energy, promote growth, etc.",
+    }, {
+        word: "Stack",
+        definition: "A pile of objects, typically one that is neatly arranged.",
+    }, {
+        word: "School",
+        definition: "An institution where instruction is given, especially to persons under college age:",
+    }, {
+        word: "Internet",
+        definition: "A vast computer network linking smaller computer networks worldwide (usually preceded by the). ",
+    }
+];
---
+++ public/controller/controller.js
@@ -0,0 +1,170 @@
+// Execute functions when page is loaded
+window.onload=function() {
+    createWelcomeMessage();
+    createButtons();
+    randomDictionaryWord();
+    populateWord();
+}
+
+// Welcome message on screen
+function createWelcomeMessage(){
+    document.getElementById("welcomeMessage").innerHTML = welcomeTitle;
+}
+
+// Create buttons for the alphabet keys and restart game
+function createButtons(){
+    // alphabet buttons  
+   for (var i = 0; i < alphabet.length; i++) {
+    var button = document.createElement("button");
+    button.classList = ['keyboard btn btn-primary btn-lg'];
+    button.addEventListener("click", letterClicked);
+    var text = document.createTextNode(alphabet[i]);
+    button.appendChild(text);
+    document.getElementById("main").appendChild(button);
+   }
+   // restart button
+   var restartButton = document.createElement("button");
+   restartButton.classList = ['btn btn-warning'];
+   var text = document.createTextNode("Restart Game");
+   restartButton.appendChild(text);
+   document.getElementById("restartButton").appendChild(restartButton);  
+   restartButton.addEventListener("click", restartGame);
+}
+
+// The letter clicked is being guessed if it is in the word
+function letterClicked(){
+    var letter = this.innerHTML;  
+    // iterate each letter in the word 
+    for(var i = 0; i < guessWord.length; i++) { 
+        // found character match
+        if (letter == guessWord.charAt(i).toUpperCase()){ 
+            displayLetter(i, letter);
+            // if letter hasn't been guessed yet 
+            // if(lettersGuessedCorrect.indexOf(letter) == -1) {
+                lettersGuessedCorrect.push(letter);
+                incrementScore();
+            // } 
+        } 
+    }
+    // if character match hasn't been found
+    if(lettersGuessedCorrect.indexOf(letter) === -1) {
+        decrementLifeScore();
+        decrementScore();
+    }
+    this.disabled = true;
+    this.classList = ['keyboard btn btn-default btn-lg'];
+    checkIfGameOver();
+    checkIfWordCompleted();
+}
+
+// Generate a random word to be guessed
+function randomDictionaryWord() {
+    var randomNumber = Math.floor(Math.random() * 9); // Random 0 to 9 
+    document.getElementById("definition").innerHTML = dictionary[randomNumber].definition;
+    guessWord = dictionary[randomNumber].word;
+}
+
+// Display the random word on the page
+function populateWord() {
+    var wordLength = guessWord.length;
+    var guessWordElement = document.getElementById("guessWord");
+
+    for(var i = 0; i < wordLength; i++) {
+        var span = document.createElement("span");
+        span.id = ['characterToGuess' + i];
+        var text = document.createTextNode("_ ");
+        span.appendChild(text);
+        guessWordElement.appendChild(span);
+    }
+}
+
+// Minus one point on the Life Count
+function decrementLifeScore() {
+    document.getElementById("lifeCount").innerHTML = --lifeCount;
+}
+
+// Add one point to the Score Count
+function incrementScore() {
+    document.getElementById("scoreCount").innerHTML = ++scoreCount;
+}
+
+// Minus one point on the Score Count
+function decrementScore() {
+    document.getElementById("scoreCount").innerHTML = --scoreCount;
+}
+
+// Display the letter if there is a match
+function displayLetter(index, letter) {
+    document.getElementById("characterToGuess" + index).innerHTML = letter;
+}
+
+// Clearing the guess word on page and letters guessed letters in the array
+function clearWord(){
+    document.getElementById("guessWord").innerHTML = "";
+    lettersGuessedCorrect = [];
+}
+
+// Clearing the message result on page
+function clearMessageResult() {
+    document.getElementById("messageResult").innerHTML = "";
+}
+
+// Resetting the life count back to 7
+function resetLifeCount() {
+    lifeCount = 7;
+    document.getElementById("lifeCount").innerHTML = lifeCount;   
+}
+
+// Reset all the buttons that were disabled back to enabled
+function resetButtons(){
+    var keys = document.getElementsByClassName("keyboard").length;
+    for(var i = 0; i < keys; i++) {
+        document.getElementsByClassName("keyboard")[i].disabled = false;
+        document.getElementsByClassName("keyboard")[i].classList = ['keyboard btn btn-primary btn-lg'];   
+    }
+}
+
+// Restarting the game by continueing
+function restartGame() {
+    continuePlaying();
+}
+
+// Reset the game if wanting the game to continue with next word to guess
+function continuePlaying() {
+    clearWord();
+    clearMessageResult();
+    resetLifeCount();
+    randomDictionaryWord();
+    populateWord();
+    resetButtons();
+}
+
+// Display a game over message if life count reached to 0 and disable all buttons
+function checkIfGameOver() {
+    if(lifeCount == 0) {
+        document.getElementById("messageResult").innerHTML = loseMessage;
+        disableAllButtons();
+    }
+}
+
+// Check if all the letters are a match for the guess word and display a congratulation message
+function checkIfWordCompleted() {
+    currentWord = "";
+    // retrieves the current word that is being displayed
+    for(var i = 0; i < guessWord.length; i++){
+        currentWord+=document.getElementById("characterToGuess" + i).innerHTML;
+    }
+    // checks if the current word is a match of the word being guessed
+    if(currentWord == guessWord.toUpperCase()) {
+        document.getElementById("messageResult").innerHTML = winMessage;
+        disableAllButtons();
+    }
+}
+
+// Disables all the buttons
+function disableAllButtons() {
+    var keys = document.getElementsByClassName("keyboard").length;
+    for(var i = 0; i < keys; i++) {
+        document.getElementsByClassName("keyboard")[i].disabled = true;
+    }
+}
