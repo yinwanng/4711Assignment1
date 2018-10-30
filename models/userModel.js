1c3b24df77b95d01d965a1422004b43255f50479
yinwanng <xxjohnnyng@gmail.com>
2018-10-29 23:15:44 -0700

    authentication

--- models/userModel.js
+++ models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrpyt = require('bcrypt');
 
 const UserSchema = new mongoose.Schema({
     email: {
@@ -18,3 +19,23 @@ const UserSchema = new mongoose.Schema({
         required: true
     }
 }); 
+
+UserSchema.statics.authenticate = (email, password, callback) => {
+    User.findOne({email: email}).exec(function( err, user){
+        if(err) {
+            return callback(err);
+        } else if(!user) {
+            var err = new Error("The user cannot be found.");
+            err.status = 401;
+            return callback(err);
+        }
+        
+        bcrypt.compare(password, user.password, function (err, result) {
+            if(result === true) {
+                return callback(null, user);
+            } else {
+                return callback();
+            }
+        })
+    });
+}
