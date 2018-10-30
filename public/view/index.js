a6caabb7a0bc6cb5fa55d34fc3b193b2678cb90e
yinwanng <xxjohnnyng@gmail.com>
2018-10-29 22:32:59 -0700

    finish setup for login

--- public/view/index.js
+++ public/view/index.js
@@ -8,6 +8,7 @@ setupLogin = () => {
     let br = document.createElement("br");
     let br2 = document.createElement("br");
     let br3 = document.createElement("br");
+    let br4 = document.createElement("br");
 
     container.appendChild(form);
 
@@ -16,14 +17,17 @@ setupLogin = () => {
     emailLabel.appendChild(emailText);
     
     let emailInput = document.createElement("input");
-    // emailLabel.appendChild(emailInput);
+    emailInput.required = true;
+    emailInput.name = "loginEmail";
     
 
     let passwordLabel = document.createElement("label");
     let passwordText = document.createTextNode("Password");
     passwordLabel.appendChild(passwordText);
     let passwordInput = document.createElement("input");
-    // passwordLabel.appendChild(passwordInput);
+    passwordInput.required = true;
+    passwordInput.name = "loginPassword";
+    
     
     form.appendChild(emailLabel);
     form.appendChild(br);
@@ -32,7 +36,11 @@ setupLogin = () => {
     form.appendChild(passwordLabel);
     form.appendChild(br3);
     form.appendChild(passwordInput);
-    
-    
+
+    let loginButton = document.createElement("button");
+    let loginText = document.createTextNode("Login");
+    loginButton.appendChild(loginText);
+    form.appendChild(br4);
+    form.appendChild(loginButton);
     
 }
