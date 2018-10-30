598628eabe2c6f55759060dea334d20e44f335a4
yinwanng <xxjohnnyng@gmail.com>
2018-10-29 23:00:22 -0700

    setup user schema

---
+++ models/userModel.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+}); 
