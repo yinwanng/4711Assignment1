ff3938d648ca2e77a9dc6b807b01c9f79ad35fd1
yinwanng <xxjohnnyng@gmail.com>
2018-10-31 01:04:29 -0700

    test

--- app.js
+++ app.js
@@ -14,9 +14,9 @@ const MongoStore = require('connect-mongo')(session);
 
 
 //live
-mongoose.connect("mongodb://admin:admin@cluster0-shard-00-00-ddn31.mongodb.net:27017,cluster0-shard-00-01-ddn31.mongodb.net:27017,cluster0-shard-00-02-ddn31.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true", { useNewUrlParser: true })
-.then(()=> console.log("Connected to MongoDB...."))
-.catch(err => console.error('Could not connect to MongoDB...'));
+mongoose.connect("mongodb://admin:admin@cluster0-shard-00-00-ddn31.mongodb.net:27017,cluster0-shard-00-01-ddn31.mongodb.net:27017,cluster0-shard-00-02-ddn31.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true", { useNewUrlParser: true });
+// .then(()=> console.log("Connected to MongoDB...."))
+// .catch(err => console.error('Could not connect to MongoDB...'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
