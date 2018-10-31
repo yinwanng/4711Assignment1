474633831e36b4b85b1b69bc188c0e9ac67bc361
yinwanng <xxjohnnyng@gmail.com>
2018-10-31 01:02:34 -0700

    live connection test

--- app.js
+++ app.js
@@ -8,7 +8,13 @@ const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
 
 // connect to mongodb
-mongoose.connect("mongodb://localhost:27017/data", { useNewUrlParser: true })
+// mongoose.connect("mongodb://localhost:27017/data", { useNewUrlParser: true })
+// .then(()=> console.log("Connected to MongoDB...."))
+// .catch(err => console.error('Could not connect to MongoDB...'));
+
+
+//live
+mongoose.connect("mongodb://admin:admin@cluster0-shard-00-00-ddn31.mongodb.net:27017,cluster0-shard-00-01-ddn31.mongodb.net:27017,cluster0-shard-00-02-ddn31.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true", { useNewUrlParser: true })
 .then(()=> console.log("Connected to MongoDB...."))
 .catch(err => console.error('Could not connect to MongoDB...'));
 
