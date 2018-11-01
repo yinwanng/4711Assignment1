596c7692f7939a03534af4ef9612882eeb5b6119
yinwanng <xxjohnnyng@gmail.com>
2018-11-01 01:36:50 -0700

    activate production mongoose

--- app.js
+++ app.js
@@ -8,13 +8,13 @@ const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
 
 // connect to mongodb localhost for development/testing
-mongoose.connect("mongodb://localhost:27017/data", { useNewUrlParser: true })
-.then(()=> console.log("Connected to MongoDB...."))
-.catch(err => console.error('Could not connect to MongoDB...'));
-mongoose.set('useCreateIndex', true);
+// mongoose.connect("mongodb://localhost:27017/data", { useNewUrlParser: true })
+// .then(()=> console.log("Connected to MongoDB...."))
+// .catch(err => console.error('Could not connect to MongoDB...'));
+// mongoose.set('useCreateIndex', true);
 
 // ->> connect to mongodb live for production environment
-// mongoose.connect("mongodb://admin:admin@cluster0-shard-00-00-ddn31.mongodb.net:27017,cluster0-shard-00-01-ddn31.mongodb.net:27017,cluster0-shard-00-02-ddn31.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true", { useNewUrlParser: true })
+mongoose.connect("mongodb://admin:admin@cluster0-shard-00-00-ddn31.mongodb.net:27017,cluster0-shard-00-01-ddn31.mongodb.net:27017,cluster0-shard-00-02-ddn31.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true", { useNewUrlParser: true })
 // .then(()=> console.log("Connected to MongoDB...."))
 // .catch(err => console.error('Could not connect to MongoDB...'));
 
