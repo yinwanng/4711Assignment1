7d527f656a2d4c0d9fd7634acd5f0ce6952a36ef
yinwanng <xxjohnnyng@gmail.com>
2018-11-01 00:42:33 -0700

    modified registration css

--- public/controller/registrationLoginController.js
+++ public/controller/registrationLoginController.js
@@ -2,3 +2,4 @@ window.onload = () => {
     setupLogin();
     setupRegistration();
 }
+
--- public/view/registrationLoginView.js
+++ public/view/registrationLoginView.js
@@ -1,44 +1,57 @@
 setupLogin = () => {
+
     let container = document.getElementById("container");
     let form = document.createElement("form");
     form.method = "post";
     form.action = "/";
     let br = document.createElement("br");
     let br2 = document.createElement("br");
-    let br3 = document.createElement("br");
-    let br4 = document.createElement("br");
-
+    form.classList = ["form-group row"];
     container.appendChild(form);
 
     let emailLabel = document.createElement("label");
     let emailText = document.createTextNode("E-mail");
     emailLabel.appendChild(emailText);
-    
+    emailLabel.classList = ["col-form-label"];
     let emailInput = document.createElement("input");
     emailInput.required = true;
     emailInput.name = "loginEmail";
+    emailInput.classList = ["form-control"];
+    emailInput.placeholder = "Enter e-mail";
     
     let passwordLabel = document.createElement("label");
     let passwordText = document.createTextNode("Password");
     passwordLabel.appendChild(passwordText);
+    passwordLabel.classList = ["col-form-label"];
     let passwordInput = document.createElement("input");
     passwordInput.required = true;
     passwordInput.name = "loginPassword";
-    
+    passwordInput.classList = ["form-control"];
+    passwordInput.placeholder = "Enter password";
     
     form.appendChild(emailLabel);
-    form.appendChild(br);
     form.appendChild(emailInput);
-    form.appendChild(br2);
     form.appendChild(passwordLabel);
-    form.appendChild(br3);
     form.appendChild(passwordInput);
 
     let loginButton = document.createElement("button");
     let loginText = document.createTextNode("Login");
+
+    loginButton.classList = ["btn btn-primary"];
+    loginButton.id = "loginButton";
     loginButton.appendChild(loginText);
-    form.appendChild(br4);
+    form.appendChild(br);
+    form.appendChild(br2);
     form.appendChild(loginButton);
+
+    let signUpButton = document.createElement("button");
+    signUpButton.classList = ["btn btn-secondary"];
+    let signUpText = document.createTextNode("Don't have an account? Click here to sign up!");
+    signUpButton.appendChild(signUpText);
+    signUpButton.addEventListener("click", signMeUp);
+    form.appendChild(signUpButton);
+
+    
 }
 
 setupRegistration = () => {
@@ -46,64 +59,65 @@ setupRegistration = () => {
     let form = document.createElement("form");
     form.method = "post";
     form.action = "/";
+    form.classList = ["form-group row"];
+    form.style.display = "none";
+
     let br = document.createElement("br");
     let br2 = document.createElement("br");
-    let br3 = document.createElement("br");
-    let br4 = document.createElement("br");
-    let br5 = document.createElement("br");
-    let br6 = document.createElement("br");
-    let br7 = document.createElement("br");
-    let br8 = document.createElement("br");
 
     container.appendChild(form);
 
     let emailLabel = document.createElement("label");
+    emailLabel.classList = ["col-form-label"];
     let emailText = document.createTextNode("E-mail");
     emailLabel.appendChild(emailText);
     let emailInput = document.createElement("input");
     emailInput.required = true;
     emailInput.name = "email";
+    emailInput.classList = ["form-control"];
 
     let usernameLabel = document.createElement("label");
+    usernameLabel.classList = ["col-form-label"];
     let usernameText = document.createTextNode("Username");
     usernameLabel.appendChild(usernameText);
     let usernameInput = document.createElement("input");
     usernameInput.required = true;
     usernameInput.name = "username";
+    usernameInput.classList = ["form-control"];
 
     let passwordLabel = document.createElement("label");
+    passwordLabel.classList = ["col-form-label"];
     let passwordText = document.createTextNode("Password");
     passwordLabel.appendChild(passwordText);
     let passwordInput = document.createElement("input");
     passwordInput.required = true;
     passwordInput.name = "password";
+    passwordInput.classList = ["form-control"];
 
     let passwordConfirmationLabel = document.createElement("label");
+    passwordConfirmationLabel.classList = ["col-form-label"];
     let passwordConfirmationText = document.createTextNode("Confirm Password");
     passwordConfirmationLabel.appendChild(passwordConfirmationText);
     let passwordConfirmationInput = document.createElement("input");
     passwordConfirmationInput.required = true;
     passwordConfirmationInput.name = "passwordConfirmation";
+    passwordConfirmationInput.classList = ["form-control"];
 
     form.appendChild(usernameLabel);
-    form.appendChild(br);
     form.appendChild(usernameInput);
-    form.appendChild(br2);
     form.appendChild(emailLabel);
-    form.appendChild(br3);
     form.appendChild(emailInput);
-    form.appendChild(br4);
     form.appendChild(passwordLabel);
-    form.appendChild(br5);
     form.appendChild(passwordInput);
-    form.appendChild(br6);
     form.appendChild(passwordConfirmationLabel);
-    form.appendChild(br7);
     form.appendChild(passwordConfirmationInput);
 
     let loginButton = document.createElement("button");
     let loginText = document.createTextNode("Register");
+    loginButton.classList = ["btn btn-primary"];
+
     loginButton.appendChild(loginText);
-    form.appendChild(br8);
+    form.appendChild(br);
+    form.appendChild(br2);
     form.appendChild(loginButton);
 }
