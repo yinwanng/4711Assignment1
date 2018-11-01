498590209df74ca2e91e10fda24b2341321d6f90
yinwanng <xxjohnnyng@gmail.com>
2018-11-01 01:44:03 -0700

    allow cors

--- app.js
+++ app.js
@@ -22,6 +22,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 var routes = require('./routes/routers');
 
 app.use(session({
