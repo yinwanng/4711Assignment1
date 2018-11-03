7dc3a563f3d0090e99b5d6fda7043c98371deee0
yinwanng <xxjohnnyng@gmail.com>
2018-11-03 01:03:07 -0700

    updated comments on user

--- public/models/User.js
+++ public/models/User.js
@@ -1,22 +1,27 @@
 // Initializing a class definition
 class User {
+    // constructor
     constructor(username, score) {
         this.username = username;
         this.score = score;
     }
 
+    // retrieve username
     getUsername() {
         return `${this.username}`;
     }
 
+    // retrieve the score
     getScore() {
         return `${this.score}`;
     }
 
+    // set the username
     setUsername(username) {
         this.username = username;
     }
 
+    // set the score
     setScore(score) {
         this.score = score;
     }
