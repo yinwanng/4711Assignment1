f9cbc11db141253e4506cc0973796621a237711f
yinwanng <xxjohnnyng@gmail.com>
2018-11-03 01:02:27 -0700

    updated comments on string

--- public/models/string.js
+++ public/models/string.js
@@ -1,8 +1,8 @@
-const msgWelcome = "Welcome to Hangman";
-const msgEnterValidEmailRegistration = "- Please enter a valid email address for registration.";
-const msgUsernameOrEmailTaken = "The username or e-mail has already been taken.";
-const winMessage = "You got it right!";            // display win message
-const loseMessage = "Game Over!";                  // display lose message
-const msgLogin = "Login to our game";
-const msgRegistration = "Sign up now";
-const msgToUser = "Beat the top score ";
+const msgWelcome = "Welcome to Hangman";                                                            // welcome message for hangman
+const msgEnterValidEmailRegistration = "- Please enter a valid email address for registration.";    // email validation on registration
+const msgUsernameOrEmailTaken = "The username or e-mail has already been taken.";                   // username or email taken on registration
+const winMessage = "You got it right!";                                                             // display win message
+const loseMessage = "Game Over!";                                                                   // display lose message
+const msgLogin = "Login to our game";                                                               // login message
+const msgRegistration = "Sign up now";                                                              // sign up message
+const msgToUser = "Beat the top score ";                                                            // the message to user when logged in
